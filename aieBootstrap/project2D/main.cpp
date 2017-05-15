65597dfbb2c73b766d580353fe7e6d155770883d
richard-stern <richards@aie.edu.au>
2017-05-15 11:05:15 +1000

    My new project

---
+++ aieBootstrap/project2D/main.cpp
@@ -0,0 +1,14 @@
+#include "Application2D.h"
+#include <crtdbg.h>
+
+int main() 
+{
+	//At start of main()
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	Application2D* app = new Application2D();
+	app->run("AIE", 1280, 720, false);
+	delete app;
+
+	return 0;
+}
